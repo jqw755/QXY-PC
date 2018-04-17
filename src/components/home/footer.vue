5dd61b786de29a609ea77b71ecc82915b1803249
Tong  CAO <tong.cao@adenservices.com>
2018-04-17 18:03:55 +0800

    页面UI修改
    底部footer修改

--- src/components/home/footer.vue
+++ src/components/home/footer.vue
@@ -1,13 +1,15 @@
 <template>
   <div class="footer">
     <div class="qrcode">
-      <div class="qrcodeleft"> 
-        <div class="web_logo" ></div>
-        <div class="i1">聪明的人，都会省钱</div>
-        <div class="i2">购物省钱神器</div>
-      </div>
-      <div class="qrcoderight">
-        <img src="./../../assets/qrcode.png">
+      <div class="qrcode-content">
+        <div class="qrcodeleft"> 
+          <div class="web_logo" ></div>
+          <div class="i1">聪明的人，都会省钱</div>
+          <div class="i2">购物省钱神器</div>
+        </div>
+        <div class="qrcoderight">
+          <img src="./../../assets/qrcode.png">
+        </div>
       </div>
     </div>
     <div class="copyright">
@@ -25,46 +27,48 @@
     line-height: 155px;
     background: #434446;
     position: relative;
-    .qrcodeleft {
-      width: 130px;
+    .qrcode-content {
+      width: 250px;
       height: 100px;
       line-height: 100px;
       position: absolute;
-      // right: 348px;
-      left: 866px;
+      left: 66.8%;
       top: 39px;
-      .web_logo {
-        position: absolute;
+      display: flex;
+      justify-content: space-between;
+      .qrcodeleft {
         width: 130px;
-        height: 41px;
-        background: url("./../../assets/quanxiaoyou_web_logo.png");
+        height: 100px;
+        line-height: 100px;
+        .web_logo {
+          position: absolute;
+          width: 130px;
+          height: 41px;
+          background: url("./../../assets/quanxiaoyou_web_logo.png");
+        }
+        div {
+          text-align: right;
+          width: 130px;
+          height: 18px;
+          line-height: 18px;
+          font-size: 14px;
+          color: #ffffff;
+          letter-spacing: 0;
+        }
+        .i1 {
+          position: absolute;
+          top: 47px;
+        }
+        .i2 {
+          position: absolute;
+          top: 71px;
+          font-size: 12px;
+        }
       }
-      div {
-        text-align: right;
-        width: 130px;
-        height: 18px;
-        line-height: 18px;
-        font-size: 14px;
-        color: #ffffff;
-        letter-spacing: 0;
+      .qrcoderight {
+        width: 100px;
+        height: 100px;
       }
-      .i1 {
-        position: absolute;
-        top: 47px;
-      }
-      .i2 {
-        position: absolute;
-        top: 71px;
-        font-size: 12px;
-      }
-    }
-    .qrcoderight {
-      width: 100px;
-      height: 100px;
-      position: absolute;
-      top: 28px;
-      left: 1016px;
-      // right: 228px;
     }
   }
   .copyright {
